df3a8d6b268035b2b863eceb76f538284fb60066
kulend <kulend@qq.com>
2018-08-09 16:39:51 +0800

    none

---
+++ src/Dnc.Api.Throttle/Memory/Cache/MemoryCacheOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dnc.Api.Throttle.Memory.Cache
+{
+    public class MemoryCacheOptions
+    {
+        /// <summary>
+        /// Key Prefix
+        /// </summary>
+        public string KeyPrefix { set; get; }
+    }
+}
--- src/Dnc.Api.Throttle/Memory/Cache/MemoryCacheProvider.cs
+++ src/Dnc.Api.Throttle/Memory/Cache/MemoryCacheProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,15 @@ namespace Dnc.Api.Throttle.Memory.Cache
 {
     internal class MemoryCacheProvider : ICacheProvider
     {
+        private readonly IMemoryCache _cache;
+        private readonly MemoryCacheOptions _options;
+
+        public MemoryCacheProvider(IMemoryCache cache, IOptions<MemoryCacheOptions> options)
+        {
+            _cache = cache;
+            _options = options.Value;
+        }
+
         public Task AddApiRecordAsync(string api, Policy policy, string policyKey, string policyValue, DateTime now, int duration)
         {
             throw new NotImplementedException();
@@ -17,8 +28,11 @@ namespace Dnc.Api.Throttle.Memory.Cache
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ListItem>> GetRosterListAsync(RosterType rosterType, string api, Policy policy, string policyKey)
+        public async Task<IEnumerable<ListItem>> GetRosterListAsync(RosterType rosterType, string api, Policy policy, string policyKey)
         {
+            var key = FromatRosterKey(rosterType, api, policy, policyKey);
+
+            var result = await Task.FromResult((IEnumerable<ListItem>)_cache.Get(key));
 
             throw new NotImplementedException();
         }
@@ -26,6 +40,17 @@ namespace Dnc.Api.Throttle.Memory.Cache
         public Task ClearRosterListCacheAsync(RosterType rosterType, string api, Policy policy, string policyKey)
         {
             throw new NotImplementedException();
+        }
+
+        private string FromatRosterKey(RosterType rosterType, string api, Policy policy, string policyKey)
+        {
+            var key = $"{_options.KeyPrefix}:{rosterType.ToString().ToLower()}:{policy.ToString().ToLower()}";
+            if (!string.IsNullOrEmpty(policyKey))
+            {
+                key += ":" + Common.EncryptMD5Short(policyKey);
+            }
+            key += ":" + api.ToLower();
+            return key;
         }
     }
 }
